d91b76fb06c9bb0fbb1b5571431ff36f870abbad
parag koche <paragkoche0@gmail.com>
2023-06-11 22:58:42 +0530

    ok

--- script/global.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var header = document.getElementById('myHeader');
-    if (window.pageYOffset > 0) {
-      header.style.height = '120px';
-    } else {
-      header.style.height = '198px';
-    }
-  });
-  
